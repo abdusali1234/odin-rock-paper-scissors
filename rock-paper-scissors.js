90a828ad926d31c808a4a2aca45447c4d8af4f5f
abdusali1234 <abdusali1234@gmail.com>
2024-06-05 12:17:39 +0100

    Updated rps to include buttons that are used to play the round

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,4 +1,4 @@
-console.log("Let's play Rock Paper Scissors!");
+
 
 const choices = ["Rock", "Paper", "Scissors"];
 function getComputerChoice() {
@@ -7,70 +7,87 @@ function getComputerChoice() {
 }
 
 
-function getHumanChoice() {
-    let choice = prompt("Please enter Rock, Paper, or Scissors");
-    if (choice[0] == null) {
-        alert("Please enter a valid term!");
-        getHumanChoice();
-        }
-     else {
-        const cleanedChoice = choice[0].toUpperCase() + choice.toLowerCase().slice(1);
-        if (cleanedChoice == "Rock" || cleanedChoice == "Paper" || cleanedChoice == "Scissors") {
-            return cleanedChoice;
-        } else {
-            alert("Please enter a valid term!");
-            getHumanChoice();
+const rockBtn  = document.querySelector("#rock");
+const paperBtn = document.querySelector("#paper");
+const scissorsBtn = document.querySelector("#scissors");
+
+const runningScore = document.querySelector("#runningScore")
+const roundScore = document.querySelector("#Roundscore");
+const human = document.querySelector("#human");
+const computer = document.querySelector("#computer");
+const finalScore = document.querySelector("#finalScore");
+
+let humanScore = 0;
+let computerScore = 0;
+const targetScore = 5;
+let isGameOver = false;
+
+rockBtn.addEventListener("click", function() {
+    if (!isGameOver){
+        playRound("Rock", getComputerChoice());
+        endGame();
     }
-}
-}
+});
 
 
-
-
-
-
-
-function playGame () {
-    const numRounds = 5;
-    let humanScore = 0;
-    let computerScore = 0;
-
-    function playRound(humanChoice, computerChoice) {
-
-        if ((humanChoice == "Rock" && computerChoice == "Scissors" 
-            || (humanChoice == "Scissors" && computerChoice == "Paper")) 
-            || (humanChoice == "Paper" && computerChoice == "Rock")) {
-            alert(`You win, ${humanChoice} beats ${computerChoice}!`);
-            humanScore++;
-        }
-        else if (humanChoice == computerChoice) {
-            alert(`Draw, both picked ${humanChoice}`);
-        } else {
-            alert(`You lose, ${computerChoice} beats ${humanChoice}!`);
-            computerScore++
-        }
+paperBtn.addEventListener("click", function() {
+    if (!isGameOver){
+        playRound("Paper", getComputerChoice());
+        endGame();
     }
+});
 
-    for (i = 0; i < numRounds; i++) {
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
+scissorsBtn.addEventListener("click", function() {
+    if (!isGameOver){
+        playRound("Scissors" , getComputerChoice());
+        endGame();
     }
+});
+
+
+
+
+
+
+
+
     
-    let gameOverString = ``;
-    if (humanScore < computerScore) {
-        gameOverString = `Computer score: ${computerScore}. Your score: ${humanScore}.
-You win!`
-    }
-    else if (humanScore == computerScore){
-        gameOverString = `Computer score: ${computerScore}. Your score: ${humanScore}.
-It's a draw!`
-    } else {
-        gameOverString = `Computer score: ${computerScore}. Your score: ${humanScore}.
-You lose!`
-    }
 
-    alert(gameOverString);
+function playRound(humanChoice, computerChoice) {
+    const newLi = document.createElement("li");
+    newLi.classList.add("newLi");
+    if ((humanChoice === "Rock" && computerChoice === "Scissors" 
+        || (humanChoice === "Scissors" && computerChoice === "Paper")) 
+        || (humanChoice === "Paper" && computerChoice === "Rock"))
+        {
+        newLi.innerText = `You win, ${humanChoice} beats ${computerChoice}!`;
+        roundScore.appendChild(newLi);
+        humanScore++;
+        human.innerText = humanScore;
+    }
+    else if (humanChoice == computerChoice) {
+        newLi.innerText = `Draw, both picked ${humanChoice}`;
+        roundScore.appendChild(newLi);
+    } else {
+        newLi.innerText = `You lose, ${computerChoice} beats ${humanChoice}!`;
+        roundScore.appendChild(newLi);
+        computerScore++;
+        computer.innerText = computerScore;
+    }
 }
 
-playGame();
+
+
+function endGame() {
+    if (humanScore === targetScore || computerScore === targetScore){
+        isGameOver = true;
+    }
+    let gameOverString = ``;
+    if (humanScore === targetScore) {
+        gameOverString = `You won the game! Poggers!`;
+    } 
+    else if (computerScore === targetScore){
+        gameOverString = `You lost the game, F in the chat.`;
+    }
+    finalScore.innerText = gameOverString;
+}
